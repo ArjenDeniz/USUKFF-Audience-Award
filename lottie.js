6b5d6e1b0338827e545f2b85b8bdd6688af7333a
sarbend <arjendenizturkmen@gmail.com>
2024-03-13 12:28:36 +0300

    failed lottie animation, background & colors added

--- lottie.js
+++ lottie.js
@@ -1,28 +1,12 @@
-function addAnimation(buttonNumber, animationNumber, path) {
-  const buttonX = document.getElementById(buttonNumber);
-  const XAnimation = document.getElementById(animationNumber);
+const animationContainer = document.getElementById("hide");
+const animation = lottie.loadAnimation({
+  container: animationContainer,
+  renderer:'svg',
+  loop: false,
+  autoplay: false,
+  path: 'https://lottie.host/47188d88-352b-45df-bd79-199f404d832e/WFnpQkLRxa.json'
+});
 
-  const animItemX = bodymovin.loadAnimation({
-    wrapper: XAnimation,
-    animType: "svg",
-    loop: false,
-    autoplay: false,
-    path: path,
-  });
-
-  buttonX.addEventListener("click", () => {
-    XAnimation.classList.remove("hide");
-    animItemX.goToAndPlay(0, true);
-  });
-
-  animItemX.addEventListener("complete", () => {
-    XAnimation.classList.add("hide");
-  });
-}
-
-addAnimation(
-  "buttonOne",
-  "lottieAnimation",
-  "https://lottie.host/47188d88-352b-45df-bd79-199f404d832e/WFnpQkLRxa.json"
-);
-
+document.getElementById("buttonOne").addEventListener('click', function(){
+  animation.play()
+});
